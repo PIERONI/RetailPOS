b7f902b81a68402c03ccda028d0c8934b1adc051
nareshphuloria <naresh.phuloria@gmail.com>
2013-05-21 00:57:21 +0530

    Initial Source code committed

---
+++ RetailPOS/RetailPOS.BusinessLayer.ServiceImpl/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RetailPOS.BusinessLayer.ServiceImpl
+{
+    public class Class1
+    {
+    }
+}
---
+++ RetailPOS/RetailPOS.PersistenceLayer.Repository/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RetailPOS.PersistenceLayer.Repository
+{
+    public class Class1
+    {
+    }
+}

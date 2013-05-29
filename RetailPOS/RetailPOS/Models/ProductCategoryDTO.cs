72f93320270eca0b626fe7e8e7137eb06e6bb1c7
sidtsharma <asm.sushil@gmail.com>
2013-05-29 21:17:38 +0530

    Commit for MVVm Changes

---
+++ RetailPOS/RetailPOS/Models/ProductCategoryDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace RetailPOS.CommonLayer.DataTransferObjects
+{
+    public class ProductCategoryDTO
+    {
+        public short Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Color { get; set; }
+    }
+}

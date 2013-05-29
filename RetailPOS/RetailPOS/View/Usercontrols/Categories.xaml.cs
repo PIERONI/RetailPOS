c48fd2167fa428fa412054da257ad0bd6d5b9c43
sidtsharma <asm.sushil@gmail.com>
2013-05-29 21:16:31 +0530

    Commit for MVVM Changes

---
+++ RetailPOS/RetailPOS/View/Usercontrols/Categories.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RetailPOS.View.Usercontrols
+{
+    /// <summary>
+    /// Interaction logic for Categories.xaml
+    /// </summary>
+    public partial class Categories : UserControl
+    {
+        public delegate void CallWindow();
+        public  event CallWindow OpenNewWin;
+
+        public Categories()
+        {
+            InitializeComponent();
+            //BindCategory();
+
+        }
+
+       
+
+        
+    }
+
+    public class Cateogory
+    {
+        public string Name { get; set; }
+        public string ColorName { get; set; }
+    }
+}
